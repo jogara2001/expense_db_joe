83a2e264d868fe85fc3725860ab24553debc786d
jogara2001 <ogarajoe1@gmail.com>
2023-05-03 13:38:11 -0700

    Added starter files

---
+++ src/api/server.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+
+description = ""
+
+tags_metadata = [
+]
+
+app = FastAPI(
+    title="Expense API",
+    description=description,
+    version="0.0.1",
+    contact={
+        "name": "Joe O'Gara",
+        "email": "jogara@calpoly.edu",
+    },
+    openapi_tags=tags_metadata,
+)
+# app.include_router()
+
+@app.get("/")
+async def root():
+    return {"message": "Welcome to the Expense API. See /docs for more information."}
