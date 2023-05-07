f5ed267fb2457b56c5e00e569205ee95b63ade00
kyuusk <ziadinnavarro4@gmail.com>
2023-05-07 03:44:56 -0700

    Add query to get_expense

--- src/api/expenses.py
+++ src/api/expenses.py
@@ -6,6 +6,41 @@ from src import database as db
 
 router = APIRouter()
 
+# We can probably move these to separate files?
+def get_user(user_id: int):
+    with db.engine.connect() as conn:
+        user = conn.execute(
+            sqlalchemy.text("SELECT * FROM user WHERE user_id = :user_id"),
+            [{"user_id": user_id}]
+        ).fetchone()
+        if user is None:
+            raise HTTPException(status_code=404, detail="user not found.")
+    return user
+
+
+def get_expense(user_id: int, expense_id: int):
+    with db.engine.connect() as conn:
+        expense = conn.execute(
+            sqlalchemy.text(
+                "SELECT * FROM expense WHERE user_id = :user_id AND expense_id = :expense_id"),
+            [{"user_id": user_id, "expense_id": expense_id}]
+        ).fetchone()
+        if expense is None:
+            raise HTTPException(status_code=404, detail="expense not found.")
+    return expense
+
+
+def get_category(user_id: int, category_id: int):
+    with db.engine.connect() as conn:
+        category = conn.execute(
+            sqlalchemy.text(
+                "SELECT * FROM category WHERE user_id = :user_id AND category_id = :category_id"),
+            [{"user_id": user_id, "category_id": category_id}]
+        ).fetchone()
+        if category is None:
+            raise HTTPException(status_code=404, detail="category not found.")
+    return category
+
 
 @router.get("/user/{user_id}/expense/{expense_id}", tags=["expenses"])
 def get_expense(user_id: int, expense_id: int):
@@ -19,14 +54,17 @@ def get_expense(user_id: int, expense_id: int):
     - `category`: the user defined category of the item
     - `description`: the user defined description of the item
     """
-    json = None
-
-    with db.engine.connect() as conn:
-        result = conn.execute("").fetchone()
-        if result is None:
-            raise HTTPException(status_code=404, detail="expense not found.")
-
-    return json
+    user = get_user(user_id)
+    expense_id, _, date, cost, category_id, description = get_expense(
+        user[0], expense_id)
+    category = get_category(user[0], category_id)
+    return {
+        "cost": cost,
+        "date": date,
+        "expense_id": expense_id,
+        "category": category[1],
+        "description": description,
+    }
 
 
 @router.get("/user/{user_id}/expenses", tags=["expenses"])
